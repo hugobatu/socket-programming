b6df44c64f51d2b34b219e4dd866cb29f96c6bb3
PhKhang <trannguyenphuckhang12@gmail.com>
2023-08-08 09:32:31 +0700

    Update: rac.py for testing taking source page

---
+++ rac.py
@@ -0,0 +1,25 @@
+from rich import print
+from bs4 import BeautifulSoup
+import socket 
+
+# URLs to try:
+# example.com
+# frogfind.com
+# info.cern.ch
+host = "example.com"
+port = 80
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+sendMsg = "GET / HTTP/1.1\r\n"\
+        "Host:" + host + "\r\n\r\n"
+s.send(sendMsg.encode())
+
+data = s.recv(4096)
+
+# soup = BeautifulSoup(data.decode(), "html.parser")
+# for a in soup.findAll('img'):
+#     if (a['src'].find(host) == -1):
+#         a['src'] = host + "/" + a['src'].lstrip("/")
+
+print(data.decode())
--- sandbox.py
+++ sandbox.py
@@ -1,17 +1,18 @@
-
 import socket 
 import threading
 from rich import print
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 pageSrc = """<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A page</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>A page</title>
 </head>
 <body>
-    Hi there
+		Hi there
 </body>
 </html>"""
 
@@ -21,66 +22,107 @@ pageUrl = "http://frogfind.com/"
 host = 'localhost'
 port = 4000
 
+def proxy(url):
+	url = url.lstrip("/").rstrip("/")
+	
+	print(f"Url: {url}")
+	
+	hostPage = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	hostPage.connect((url, 80))
+	sendMsg = "GET / HTTP/1.1\r\n"\
+				"Host:" + url + "\r\n\r\n"
+	hostPage.send(sendMsg.encode())
+
+	data = hostPage.recv(4096)
+	
+	soup = BeautifulSoup(data.decode(), "html.parser")
+	for a in soup.findAll('img'):
+		if (a['src'].find(url) == -1):
+			a['src'] = "#"
+	
+	return soup
+
 def runTask(c, addr):
-  while True:
-    print("Connect from ", addr)
-    #server sử dụng kết nối gửi dữ liệu tới client dưới dạng binary
-    # c.send(b"Hello, how are you")
-    # c.send(b"Bye\nHwllo world, programmed to work but not to feel")
-    
-    msg = c.recv(1024)
-    print(msg.decode())
-    
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect(("example.com", 80))
-    server.send(b"GET / HTTP/1.1\r\nHost:www.example.com\r\n\r\n")
-    data = server.recv(4096)
-    print(f"From page: {data.decode()}")
-    
-    
-    c.send(b"HTTP/1.0 200 OK\n")
-    c.send(b"Content-Type: text/html\n")
-    c.send(b"\n")
-    c.send(b"""
-        <html>
-        <body>
-        <h1>Hello World</h1> this is my server!
-        </body>
-        </html>
-    """)
-    
-    # content = f"HTTP/1.1 200\r\nContent-Length: " + str(len(pageSrc)) + f"Content-Type: text/html\r\n" + pageSrc
-    # c.send(content.encode())
-    
-    while True:
-      str = input("Enter your line: ")
-      c.send(str.encode())
-    
-    msg = c.recv(2048)
-    print(msg.decode())
+	while True:
+		msg = c.recv(1024)
+		if (not msg):
+			c.close()
+			return
+		
+		intro = str(datetime.now()) + "| Connect from " + str(addr)
+		print(intro)
+		#server sử dụng kết nối gửi dữ liệu tới client dưới dạng binary
+		# c.send(b"Hello, how are you")
+		# c.send(b"Bye\nHwllo world, programmed to work but not to feel")
+		
+		print(msg.decode())
+		
+		msgList = msg.decode().split()
+		
+		if (msgList[0] == "GET"):
+			print("Proxy")
+			pageSrc = proxy(msgList[1])
+			print(pageSrc)
+			
+			sent = 0
+			print(f"Amount to send: {len(pageSrc)}")
+			while sent < len(pageSrc):
+				sent = sent + c.send(pageSrc.encode()[sent:])
+				print(f"Sent: {sent}")
+		
+		# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# server.connect(("example.com", 80))
+		# server.send(b"GET / HTTP/1.1\r\nHost:www.example.com\r\n\r\n")
+		# data = server.recv(4096)
+		# print(f"From page: {data.decode()}")
+		
+		
+		# c.send(b"HTTP/1.0 200 OK\n")
+		# c.send(b"Content-Type: text/html\n")
+		# c.send(b"\n")
+		# c.send(b"""
+		#     <html>
+		#     <body>
+		#     <h1>Hello World</h1> this is my server!
+		#     </body>
+		#     </html>
+		# """)
+		
+		# content = f"HTTP/1.1 200\r\nContent-Length: " + str(len(pageSrc)) + f"Content-Type: text/html\r\n" + pageSrc
+		# c.send(content.encode())
+		
+		# while True:
+		#   str = input("Enter your line: ")
+		#   c.send(str.encode())
+		
+		# msg = c.recv(2048)
+		# print(msg.decode())
+		
+		
 
 def main():
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.bind((host, port))
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host, port))
 
-  s.listen(3) # 1 ở đây có nghĩa chỉ chấp nhận 1 kết nối
-  print("Server listening on port", port)
+	s.listen(3) # 1 ở đây có nghĩa chỉ chấp nhận 1 kết nối
+	print("Server listening on port", port)
 
 
 
-  print("before the while loop")
-  while True:
-    try:
-      print("in the while loop")
-      c, addr = s.accept()
-      thread = threading.Thread(target = runTask, args = (c, addr))
-      thread.start()
-      
-    except KeyboardInterrupt:
-      print("Closing program")
-      c.close()
+	print("before the while loop")
+	while True:
+		try:
+			print("in the while loop")
+			c, addr = s.accept()
+			# thread = threading.Thread(target = runTask, args = (c, addr))
+			# thread.start()
+			runTask(c, addr)
+			
+		except KeyboardInterrupt:
+			print("Closing program")
+			c.close()
 
-    
+		
 if __name__ == "__main__":
-  main()
-    
+	main()
+		
