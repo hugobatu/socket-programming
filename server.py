311566c5ffd6f7051d2531dc84a7b4492e716f44
PhKhang <trannguyenphuckhang12@gmail.com>
2023-08-18 22:07:17 +0700

    Update: saving before removing useless files

--- server.py
+++ server.py
@@ -5,8 +5,7 @@ import json
 import os
 from rich.console import Console
 console = Console()
-from datetime import datetime
-from bs4 import BeautifulSoup
+import datetime
 
 page = """HTTP/1.1 200 OK
 Cache-Control: no-cache, no-store, must-revalidate
@@ -24,30 +23,36 @@ Content-Length: 0
 
 """
 
-pageUrl = "http://frogfind.com/"
-
 # Định nghĩa host và port mà server sẽ chạy và lắng nghe
 host = 'localhost'
 port = 4000
 
+def abridgedPrint(data = b"", leadingArrow = True):
+	if leadingArrow:
+		console.print("--> :", style="bold cyan", end="")
+		
+	if len(data) > 1200:
+		data = data[:1200] + b"..."
+		
+	try:
+		console.print(f"{data.decode()}\n", style="bold cyan")
+	except:
+		try:
+			console.print(data.encode("ISO-8859-1") + "\n", style="bold cyan")
+		except: 
+			print(f"{data}\n")
 
-# abridged function added
-def abridgedPrint(data, limit):
-	if len(data) <= limit:
-		print(data)
-	else:
-		print(data[:limit-1] + ".")
-
-
-# TODO: append to turn relative JS script to absolute - update: damn i dont need to do that
 def getConfig():
 	fileConfig = open('config.mèo')
 	configs = json.load(fileConfig)
-	return configs['cache_time'], configs['whitelisting'], configs['time']		
-global cache_time, whitelisting, allowed_time
-cache_time, whitelisting, allowed_time = getConfig()
+	return configs['cache_time'], configs['time_out'], configs['whitelisting'], configs['time'], configs['ext_to_save']
+cache_time, time_out, whitelisting, allowed_time, ext_to_save = getConfig()
 
 def createFile(fileName, content):
+	# Neu extension cua file khong nam trong ext_to_save -> Khong luu cache
+	if not (fileName[-3:] == "all" or fileName[-3:] in ext_to_save or fileName[-4:] in ext_to_save):
+		return
+	
 	try:
 		console.print(f"Creating {os.path.dirname(fileName)}", style="purple4")
 		
@@ -55,8 +60,10 @@ def createFile(fileName, content):
 		os.makedirs(os.path.dirname(fileName), exist_ok=True)
 		
 		# Ghi file
+		with open(fileName + ".header", "wb") as f:
+			f.write(content.partition(b"\r\n\r\n")[0])
 		with open(fileName, "wb") as f:
-			f.write(content)
+			f.write(content.partition(b"\r\n\r\n")[2])
 	except:
 		console.print("Wrong file path", style="red bold")
 	
@@ -64,25 +71,25 @@ def createFile(fileName, content):
 def getRequestInfo(clientRequest):
 	# Tach request cua Client tai moi dau cach, tao thanh mot mang chua cac thong tin
 	try:
-		requestList = clientRequest.decode().split()
+		requestList = clientRequest.decode("ISO-8859-1").split()
 	# Neu khong the tach duoc -> Ket thuc ham
 	except:
 		console.print("Contains gzip")
 		return	
 	
 	# Tim vi tri bat dau va ket thuc cua host, ie. Host: info.cern.ch
-	hostStartIndex = clientRequest.decode().find("Host: ") + 6
-	hostEndIndex = clientRequest.decode().find("\r\n", hostStartIndex)
+	hostStartIndex = clientRequest.decode("ISO-8859-1", "ignore").find("Host: ") + 6
+	hostEndIndex = clientRequest.decode("ISO-8859-1", "ignore").find("\r\n", hostStartIndex)
 	
 	# Cac bien de tra ve
 	method = requestList[0]
 	resource = requestList[1]
-	host = clientRequest.decode()[hostStartIndex: hostEndIndex]
+	host = clientRequest.decode("ISO-8859-1", "ignore")[hostStartIndex: hostEndIndex]
 	
 	if resource[len(resource) - 1] == "/":
 		resource = resource[:len(resource) - 1]
 	
-	# console.print(f"verb: {verb}, resource: {resource}, host: {host}")
+	# console.print(f"verb: {method}, resource: {resource}, host: {host}")
 	
 	return method, resource, host
 	
@@ -93,11 +100,10 @@ def checkCache(msg):
 	# Xoa http://
 	name = url.replace("http://", "")
 	
-	
-	
 	# Neu ten Resource khac ten Host -> them Resource o cuoi duong dan
 	if host != name:
 		fullPathName = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache/", host, name)
+		# fullPathName = c:/Users/phkhng/Documents/Code/socket-programming/cache/oosc.online/img/thing.png
 	# Neu giong nhau -> them index o cuoi duong dan
 	else:
 		fullPathName = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache/", host, "index")
@@ -108,84 +114,80 @@ def checkCache(msg):
 	data = b""
 	isExist = True
 	if not os.path.isfile(fullPathName):
-		console.print(f"No cache", style="deep_pink3")
+		# console.print(f"No cache", style="deep_pink3")
 		isExist = False
 	else:
+		fileInfo = datetime.datetime.fromtimestamp(os.path.getmtime(fullPathName))
+		if (datetime.datetime.today().timestamp() - fileInfo.timestamp() > cache_time):
+			return "", fullPathName, False
+		
 		console.print(f"Taking from cache: {fullPathName}", style="deep_pink3")
-		with open(fullPathName, "rb") as fp:
-			data = fp.read()	
+		with open(fullPathName + ".header", "rb") as fp:
+			data = fp.read()
+		with open(fullPathName, "rb") as pic:
+			data += b"\r\n\r\n" + pic.read()
 	
 	return data, fullPathName, isExist
 
-def proxy(url, msg):
+def proxy(msg):
 	method, url, host = getRequestInfo(msg)
-	
-	console.print(f"Url: {url}")
 	
 	cache, fullPathName, isExist = checkCache(msg)
 	
 	if (isExist):
-		cache = cache.partition(b"\r\n\r\n")[0]
-		try:
-			console.print(f"--> :{cache.decode()}", style="bold cyan")
-		# Neu co ky tu UNICODE trong data -> In tat ca duoi dang nhi phan
-		except:
-			print(f"--> :{cache}")
+		abridgedPrint(cache)
 		return cache
-	else:
-		print()
 
-	# Neu file cache cua request co ton tai -> Request nguon tu web server va luu cache
-	console.print(f"Get fresh source", style="deep_pink3")
+	# console.print(f"Get fresh source", style="deep_pink3")
 	hostPage = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	
+	hostPage.settimeout(time_out)
 	# Thu ket noi web server: Ket noi thanh cong -> Gui sendMsg toi server va nhan ket qua vao data
 	try:
 		# Ket noi toi web server
 		hostPage.connect((host, 80))
 		
-		# Tao request gui toi web server
 		if (method == "GET" or method == "HEAD"):
 			sendMsg = f"{method} {url} HTTP/1.1\r\n"\
 						"Host: " + host + "\r\n"\
 						"Connection: close\r\n\r\n"
 		elif (method == "POST"):
-			sendMsg = f"{method} {url} HTTP/1.1\r\n"
-			sendMsg += msg.decode().partition("\r\n")[2]
-			sendMsg = sendMsg[:sendMsg.find("Connection:")] + "Connection: close" + sendMsg[sendMsg.find("\r\n", sendMsg.find("Connection:")):]
+			if (msg.decode("utf-8", "ignore").partition("\r\n\r\n")[0].find("Connection:") != -1):
+				sendMsg = f"{method} {url} HTTP/1.1\r\n"
+				sendMsg += msg.decode("utf-8", "ignore").partition("\r\n")[2]
+				sendMsg = sendMsg[:sendMsg.find("Connection:")] + "Connection: close" + sendMsg[sendMsg.find("\r\n", sendMsg.find("Connection:")):]
+			else:
+				sendMsg = f"{method} {url} HTTP/1.1\r\n"
+				sendMsg += msg.decode("utf-8", "ignore").partition("\r\n")[2]
+				sendMsg.replace(b"\r\n\r\n", b"Connection: close\r\n\r\n")
+				
 		else:
-			return
+			raise Exception("Unsupported Method: " + method)
 		
-		console.print(f"Sending: {sendMsg}")
+		console.print(f"Sending: ", style="green")
+		console.print(sendMsg)
 		hostPage.send(sendMsg.encode())
 
 		# Nhan ve ket qua server vao tung mau kich thuoc 4096 -> Gop lai thanh mot bai hoan chinh
-		response = b""
+		data = b""
 		while True:
 			chunk = hostPage.recv(4096)
 			if len(chunk) == 0:     # No more data received, quitting
 				break
-			response = response + chunk
-		
-		data = response
+			data = data + chunk
 		
 		createFile(fullPathName, data)
 	
 	# Ket noi khong thanh cong -> data = page (da tao tu truoc)
 	except Exception as e:
-		print(e)
+		console.print(e, style="red")
 		data = page.encode("ISO-8859-1")
 	
 	if (method == "HEAD"):
 		data = data.partition(b"\r\n\r\n")[0]
 	
 	# In ra ket qua nhan ve tu web server
-	# Neu khong co ky tu UNICODE trong data -> Moi dung duoc .decode (chuyen tu thong tin nhi phan sang string)
-	try:
-		console.print(f"--> :{data.decode()}", style="bold cyan")
-	# Neu co ky tu UNICODE trong data -> In tat ca duoi dang nhi phan
-	except:
-		print(f"--> :{data}")
+	console.print(f"Response: ", style="green")
+	abridgedPrint(data)
 		
 	return data
 
@@ -205,56 +207,36 @@ def runTask(client, addr):
 			client.close()
 			return
 		
-		try:
-			console.print(f"Message received")
-		except:
-			console.print("UnicodeDecodeError: 'utf-8' codec can't decode byte ...")
-		
 		# In ra thong tin Client: thoi gian, dia chi IP
-		intro = str(datetime.now()) + " | Connect from " + str(addr)
-		console.print(intro, style="bold")
-		
-		# Phan tich cac thanh phan request cua Client va tra ra: 
-		# Method (GET, HEAD, POST,...),
-		# Resource (ie. frogfind.com, oosc.online/style.css,...),
-		# Host: ten trang web
-		getRequestInfo(msg)
+		console.print(str(datetime.datetime.now()) + " | Connect from " + str(addr), style="bold")
 		
 		# In request cua Client
-		try:
-			console.print(msg.decode())
-		except:
-			console.print("Cannot decode UNICODE", style="bold red")
-			return
+		abridgedPrint(msg, False)
 		
-		# 18-20
-		start = datetime.time(allowed_time.partition("-")[0], 0)
-		end = datetime.time(allowed_time.partition("-")[2], 0)
-		if not (start <= datetime.datetime.today().time() <= end):
+		# Kiem tra thoi gian truy cap
+		if (allowed_time != ""):
+			start = datetime.time(int(allowed_time.partition("-")[0]), 0)
+			end = datetime.time(int(allowed_time.partition("-")[2]), 0)
+			if not (start <= datetime.datetime.today().time() <= end):
+				console.print("Not in allowed time", style="red")
+				client.send(page.encode())
+				client.close()
+				return
+		
+		method, url, host = getRequestInfo(msg)
+		
+		# Kiem tra ten mien truy cap
+		if len(whitelisting) > 0 and not (host in whitelisting):
+			console.print("Client must not access this domain: " + host, style="red")
 			client.send(page.encode())
 			client.close()
+			return
 			
-		msgList = msg.decode().split()
-		
-		# Cac thanh phan request cua Client: 
-		# msgList[0]: Method (GET, HEAD, POST,...),
-		# msgList[0]: Resource (ie. frogfind.com, oosc.online/style.css,...),
-		# msgList[0]: Host: ten mien trang web
-		if (msgList[0] == "GET"):
-			console.print("Get")
-			pageSrc = proxy(msgList[1], msg)
+		console.print(f"Method: {method}")
+		pageSrc = proxy(msg)
+		# Gui ket qua cua web server ve lai Client
+		console.print("Bytes sent: ", client.send(pageSrc))
 			
-			# Gui ket qua cua web server ve lai Client
-			console.print("Bytes sent: ", client.send(pageSrc))
-			
-		elif (msgList[0] == "POST"):
-			console.print("Post")
-			pageSrc = proxy(msgList[1], msg)
-			
-			# Gui ket qua cua web server ve lai Client
-			console.print("Bytes sent: ", client.send(pageSrc))
-		
-		
 		client.close()
 			
 def main():
